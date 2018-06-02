cbb01d994879c6e22ee86abf2613c1b80d8f1b5b
Takayuki Kamiyama <karuma.reason@gmail.com>
2018-06-02 18:40:24 +0900

    Update,file tree change.

--- mini_test/mini_test_glob.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'minitest/autorun'
-
-class GlobTest < Minitest::Test
-  def test_glob
-   assert_equal Dir.glob('./test/*.rb'),Dir.glob('./test/*')
-  end
-end
