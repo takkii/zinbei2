cc878f5f9447f498ccd5894dd86c33e046627c54
Takayuki Kamiyama <karuma.reason@gmail.com>
2023-03-09 10:03:49 +0900

    Update.

--- mini_test/mini_test_version.rb
+++ mini_test/mini_test_version.rb
@@ -5,6 +5,10 @@ require_relative File.join(File.dirname(__FILE__), '../lib/zinbei/version')
 
 class VersionTest < Minitest::Test
   def test_version
-   assert_equal Zinbei::VERSION, '2.0.1'
+    @v1 = Zinbei::VERSION
+    @v2 = '3.0.0'
+
+    refute_equal(@v1, @v2)
+    assert_operator(@v1, :<, @v2)
   end
 end
