b9a1e3b93de41712bb6bb489a90b74b76eff05c4
Takayuki Kamiyama <karuma.reason@gmail.com>
2023-03-09 10:03:53 +0900

    Update.

--- mini_test_runner.rb
+++ mini_test_runner.rb
@@ -1,63 +1,36 @@
 # frozen_string_literal: true
 
-require 'minitest/autorun'
+require "#{File.dirname(__FILE__)}/req/minitest"
 
-puts ''
-puts ' mini_test in filename list...start '.center(60,'-')
-puts ''
-
-class MiniFilename
-
-	def mini_find
-		puts Dir.glob('./mini_test/*.rb')
-	end
+# Default encoding utf8, Encoding change here.
+def encoding_style
+  Encoding.default_internal = 'UTF-8'
+  Encoding.default_external = 'UTF-8'
 end
 
-MiniFilename.new.mini_find
+# The new instance will be deleted after process ends.
+class MiniTestFile
+  attr_reader :mini_test, :mini_unit
 
-puts ''
-puts ' mini_test in filename list...exit '.center(60,'-')
-puts ''
+  def initialize
+    encoding_style
+    @mini_test = Mini(true, '/GitHub/zinbei2/mini_test')
+    @mini_unit = Mini(true, '/GitHub/zinbei2/mini_unit')
+  end
 
-module Mini_Test_One
-
-	module_function
-	
-	def mini_test_version
-		require_relative File.join(File.dirname(__FILE__),'mini_test/mini_test_version')
-	end
-	
-	def mini_test_date 
-                require_relative File.join(File.dirname(__FILE__),'mini_test/mini_test_date')
-        end
-        
-	def mini_test_encode
-                require_relative File.join(File.dirname(__FILE__),'mini_test/mini_test_encode')
-        end
-        
-	def mini_test_glob
-                require_relative File.join(File.dirname(__FILE__),'mini_test/mini_test_glob')
-        end
-        
-	def mini_test_method
-                require_relative File.join(File.dirname(__FILE__),'mini_test/mini_test_method')
-        end
-        
-	def mini_test_sort
-                require_relative File.join(File.dirname(__FILE__),'mini_test/mini_test_sort')
-        end
-        
-	def mini_test_updown
-                require_relative File.join(File.dirname(__FILE__),'mini_test/mini_test_updown')
-    end
+  def remove
+    remove_instance_variable(:@mini_test)
+    remove_instance_variable(:@mini_unit)
+  end
 end
 
-Mini_Test_One.mini_test_version
-Mini_Test_One.mini_test_date
-Mini_Test_One.mini_test_encode
-Mini_Test_One.mini_test_glob
-Mini_Test_One.mini_test_method
-Mini_Test_One.mini_test_sort
-Mini_Test_One.mini_test_updown
+# About Exception, begin ~ rescue ~ ensure.
+begin
+  MiniTestFile.new.remove
+rescue StandardError => e
+  puts e.backtrace.tanraku_exit
+ensure
+  GC.compact
+end
 
 __END__
